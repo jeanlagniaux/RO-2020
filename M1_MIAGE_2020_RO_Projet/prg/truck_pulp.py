27a24742ce11298191945ae58d31c022e3a2160c
Jean Lagniaux <34351725+jeanlagniaux@users.noreply.github.com>
2021-01-14 20:01:41 +0100

    Update truck_pulp.py

---
+++ M1_MIAGE_2020_RO_Projet/prg/truck_pulp.py
@@ -0,0 +1,26 @@
+# -*- coding=utf-8 -*-
+"""Module `truck_pulp.py`.
+Attention à l'importation de module, explication :
+quand vous lancerez votre test, vous vous trouverez
+un étage précédent dans l'arborescence des fichiers
+par rapport au dossier contenant vos programmes.
+Par conséquent, dans chacun des programmes dans le dossier
+vous devez indiquer un chemin relatif par rapport à votre
+fichier `test.py`
+"""
+# Pour charger un module dans le dossier du projet
+import projet_RO_NOM1_PRENOM1_NOM2_PRENOM2.module as mod
+# ...
+def solve_truck_problem(file_path):
+# Faire quelque chose ici avec l'argument `file_path`
+# qui est un chemin de fichier
+# ...
+# La fonction retournera :
+# - la valeur de la fonction objectif égale aux bénéfices
+# de l'entreprise si le problème est resolvable,
+# sinon `None`. Le type de retour sera un "float" ;
+# - un dictionnaire,
+# où les clefs sont les routes et les valeurs associées
+# sont les quantités de marchandises qui les traversent ;
+# - ce que vous voulez en plus si besoin.
+return optval, roads_qty #, ...
