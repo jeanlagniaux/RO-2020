e3cc86b51e52a7291ae11084e746d975cbd162cd
OmniCyan <43406947+OmniCyan@users.noreply.github.com>
2021-02-07 12:28:13 +0100

    ugfi

--- M1_MIAGE_2020_RO_Projet/projet_RO_LAGNIAUX_JEAN_DENES_THEO/truck_pulpV2.py
+++ M1_MIAGE_2020_RO_Projet/projet_RO_LAGNIAUX_JEAN_DENES_THEO/truck_pulpV2.py
@@ -50,9 +50,13 @@ def def_truck_problem(graph, entete):
 
     list_route = [val for val in graph.edges()]
     dicts_route = {}
-    for i, j in list_route:
+    for i,j in list_route:
         dicts_route[i,j] = {'cap' : graph.edges[i,j]['capacity'], 'gas' : graph.edges[i,j]['Gas'], 'tax' : graph.edges[i,j]['Tax']}
     use_road = pl.LpVariable.dicts("Use_road", list_route, 0, 1, cat=pl.LpBinary)
+
+
+    for i in use_road:
+        print(i)
 
     truck_stock_onRoad = pl.LpVariable.dicts('raod', list_route, cat=pl.LpInteger)
 
@@ -63,8 +67,9 @@ def def_truck_problem(graph, entete):
     #erruer vient peut etre du fait qu'on déclare plusieur fois i , j
 
     #prob += pl.lpSum(1000 * use_customer[c] * customer_need[c] for c in list_customer) - pl.lpSum((use_road[(i, j)] * dicts_route[use_road[(i,j)]]['gas'] for (i, j) in list_route) + ( use_road[(i, j)] * dicts_route[use_road[(i, j)]]['tax'] * truck_stock_onRoad[(i, j)] for (i, j) in list_route))
-    prob += pl.lpSum(1000 * use_customer[c] * customer_need[c] for c in list_customer) - pl.lpSum((use_road[(i, j)] * dicts_route[use_road[(i,j)]]['gas'] + ( use_road[(i, j)] * dicts_route[use_road[(i, j)]]['tax'] * truck_stock_onRoad[(i, j)]) for (i, j) in list_route))
+    #prob += pl.lpSum(1000 * use_customer[c] * customer_need[c] for c in list_customer) - pl.lpSum((use_road[(i,j)] * dicts_route[use_road[(i,j)]]['gas'] + ( use_road[(i,j)] * dicts_route[use_road[(i,j)]]['tax'] * truck_stock_onRoad[(i,j)]) for (i,j) in list_route))
 
+    prob += pl.lpSum(1000 * use_customer[c] * customer_need[c] for c in list_customer) - pl.lpSum(([i] * dicts_route[[i]['gas']] + ( [i]  * dicts_route[[i]['tax']] * truck_stock_onRoad[(i,j)]) for i in use_road))
     # ------------------------------------------------------------------------ #
     # The constraints
     # ------------------------------------------------------------------------ #
@@ -76,11 +81,11 @@ def def_truck_problem(graph, entete):
     for d in list_depot:
         prob += use_depot[d] <= int(entete[3])
 
-    for (i, j) in list_route:
+    for (i,j) in list_route:
         prob += truck_stock_onRoad[(i,j)] <= entete[0]
 
     # stk du camion inf à la capacité de la route
-    for (i, j) in list_route:
+    for (i,j) in list_route:
         prob += truck_stock_onRoad[(i,j)] <= dicts_route[(u,v)]['capacity']
 
     for d in list_depot:
@@ -92,7 +97,7 @@ def def_truck_problem(graph, entete):
     for c in use_customer:
         prob += use_customer[c] * customer_need[c] == 0
 
-    for (i, j) in list_route:
+    for (i,j) in list_route:
         prob += pl.lpSum(use_road[(u,v)]) <= 1
 
     return prob
